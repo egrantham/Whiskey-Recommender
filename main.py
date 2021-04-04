91f868ffe1c634677fb46fa22d2e1f1daf5fe5db
egrantham <65910809+egrantham@users.noreply.github.com>
2021-04-04 12:41:05 -0400

    Cleaning Up..
    Improved Whiskey's toString method for our output
    
    modified combine method to deal with price appropriately, made use of Whiskey class

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Review():
         self.price = price
     
     def toString(self):
-        print(f'Whisky Name: {self.name}\n'+
+        return (f'Whisky Name: {self.name}\n'+
               f'Link To Reddit Review: {self.link}\n'+
               f'Reviewer Rating: {self.rating}\n'+
               f'Whisky Region or Style: {self.style}\n'+
@@ -30,10 +30,15 @@ class Whiskey():
         self.price = price
     
     def toString(self):
-        print(f'Whisky Name: {self.name}\n'+
-              f'Reviewer Rating: {self.rating}\n'+
-              f'Whisky Region or Style: {self.style}\n'+
-              f'Price Paid: {self.price}\n')
+        s = (f'Whisky Name:            {self.name}\n'+
+             f'Avg. Reviewer Rating:  {self.rating : 2.2f}\n'+
+             f'Region or Style:        {self.style}\n')
+        # this block handles the price info whether it is available or not
+        if self.price != -1.0:
+            s += (f'Avg. Price Paid:        ${self.price : 4.2f}\n')
+        else:
+            s += (f'Avg. Price Paid:        Unavailable\n')
+        return s
 
 def convert(line):
     """Converts a line of text into the args for Review class"""
@@ -43,73 +48,97 @@ def convert(line):
         line.remove('\n')
     return line
 
-def combine(reviewList, minReviews):
-    # key = name, value[0] = total rating points, value[1] = # of reviews
+def combine(reviewList, minReviews, desiredStyle, maxPrice):
+    
+    # loop to aggregate the reviews 
     whiskeyDict = {}
-    count = 0
     for review in reviewList:
-        count += 1
         if review.name in whiskeyDict.keys():
-            #print(count, review.name)
             try:
-                whiskeyDict[review.name][0] += float(review.rating)
-                whiskeyDict[review.name][1] += 1
+                whiskeyDict[review.name]['reviewTotal'] += float(review.rating)
+                whiskeyDict[review.name]['num'] += 1
             except ValueError:
                 pass
         else:
             try:
-                whiskeyDict[review.name] = [float(review.rating),1]
+                whiskeyDict[review.name] = {'reviewTotal':float(review.rating),
+                                             'num':1, 'style': review.style}
             except ValueError:
                 pass
-            
-    # here, we calculate the avg ratings using whiskeyDict         
-    avg_ratings_Dict = {key: value[0]/value[1] for key, value in whiskeyDict.items()}
     
-    # code to sort the dictionary by value
-    sorted_ratings = {}
+    # loop to aggregate the prices        
+    priceDict = {}
+    for review in reviewList:
+        if review.name in priceDict.keys():
+            if review.price != -1.0:
+                priceDict[review.name]['priceTotal'] += review.price
+                priceDict[review.name]['num'] += 1
+        else:
+            if review.price != -1.0:
+                priceDict[review.name] = {'priceTotal': review.price,
+                                             'num':1}
+            
+    # here, we calculate the avg ratings and prices using whiskeyDict and priceDict         
+    avg_ratings_Dict = {key: value['reviewTotal']/value['num'] for key, value in whiskeyDict.items()}
+    avg_price_Dict = {key: value['priceTotal']/value['num'] for key, value in priceDict.items()}
+    
+    # code to sort by rating
+    sorted_ratings = []
     sorted_keys = sorted(avg_ratings_Dict, key=avg_ratings_Dict.get)
 
     for key in sorted_keys:
-        # here, restricting the list only whiskies w/ at least 50 ratings
-        if whiskeyDict[key][1] >= minReviews:
-            sorted_ratings[key] = avg_ratings_Dict[key]    
+        # here, restricting the list only whiskies that match the desired style, price and # of reviews
+        if key in avg_price_Dict.keys():
+            if whiskeyDict[key]['num'] >= minReviews and avg_price_Dict[key] <= maxPrice and whiskeyDict[key]['style'].lower() == desiredStyle.lower() :  
+                sorted_ratings.append(Whiskey(key,avg_ratings_Dict[key],whiskeyDict[key]['style'], avg_price_Dict[key]))
+        else:
+            if whiskeyDict[key]['num'] >= minReviews and whiskeyDict[key]['style'].lower() == desiredStyle.lower() :  
+                sorted_ratings.append(Whiskey(key,avg_ratings_Dict[key],whiskeyDict[key]['style'], -1.0))
     return sorted_ratings
+
+def makeFloat(s):
+ # this function will force the price to be a float or -1.0 if invalid 
+    try:
+        price = float(s)
+    except ValueError:
+        price = -1.0
+    return price
 
 def readFile():    
     file = open("reddit_whisky_data.csv", "r", errors = 'ignore')
-    
+    # we're not doing anything with categories yet... but we might
     categories = file.readline().split(',')
     
     reviewList = []
     count = 0
     
+    # for every line, create a Review object and add it to the list
     for line in file:
         count += 1
         argList = convert(line)
         if len(argList) == 4:
             argList.append('N/A')
+        argList[4] = makeFloat(argList[4])
         review = Review(argList[0], argList[1], argList[2], argList[3], argList[4])
         reviewList.append(review)
     file.close()
     return reviewList
 
 def main():
+    # getting input
     minReviews = int(input("Minimum # of reviews? "))
-    
-    # TODO: add code to filter by style, price
     desiredStyle = input("Any region or style you are interested in? ")
-    maxPrice = input("What is the maximum price you'd pay for a bottle? ")
+    maxPrice = float(input("What is the maximum price you'd pay for a bottle? "))
 
+    # read file, combine data into list
     reviewList = readFile()
+    sorted_ratings = combine(reviewList, minReviews, desiredStyle, maxPrice)
     
-    sorted_ratings = combine(reviewList, minReviews)
-    
+    # output
     print("\n"+str(len(sorted_ratings))+" Unique Whiskies Found:\n")
-    print(sorted_ratings)
-    
-    #print(categories)
-#    for review in reviewList:
-#        review.toString()
+    for whiskey in sorted_ratings:
+        print(whiskey.toString())
+
 
 if __name__ == "__main__":
     main()
